7d8ca410d8e32b931d370852cad08194dbaa695c
Sathwik P Bhat <180687706+SathwikPBhat@users.noreply.github.com>
2025-06-23 18:30:51 +0530

    Changed connection

--- config/mongoconnect.js
+++ config/mongoconnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config(); 
 const connectmongo = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_CLOUD);
     console.log('MongoDB connection successful');
   } catch (error) {
     console.error('MongoDB connection error:', error);
